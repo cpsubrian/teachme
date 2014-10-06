83fd08f1f4392b0a8e862faab534dec613cb3763
Brian Link <cpsubrian@gmail.com>
2014-10-06 12:25:42 -0700

    Min/max height for top/bottom.

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
     connect: {
       server: {
         options: {
-          port: 3000,
+          port: 9000,
           keepalive: true
         }
       }
